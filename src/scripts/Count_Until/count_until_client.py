62989b1d70710451cf37364716dc0c6a4f89a845
GXR1LL4Z <95925265+GXR1LL4Z@users.noreply.github.com>
2022-10-25 22:46:43 +0200

    Update count_until_client.py

--- src/scripts/Count_Until/count_until_client.py
+++ src/scripts/Count_Until/count_until_client.py
@@ -1,22 +1,23 @@
 import rospy
-import actionlib
+import actionlib                            #dzieki temu mozna robic Sipleaction
 from actions.msg import CountUntilAction
-from actions.msg import CountUntilGoal
-
+from actions.msg import CountUntilGoal      #zmienna typu goal
+from actions.msg import CountUntilFeedback
 
 
 class count_until_client:
     def __init__(self):
-        self._action_client = actionlib.SimpleActionClient('/count_until', CountUntilAction)
-        self._action_client.wait_for_server()
+        self._action_client = actionlib.SimpleActionClient('/count_until', CountUntilAction)            #inicjhalizacja klienta
+        self._action_client.wait_for_server()                                                           #czeka az serwer bedzie dzialal
         rospy.loginfo("Action server is up lets gooo")
         
     def send_goal(self):
         #potrzeba CountUntilGoal
-        goal = CountUntilGoal(max_numberf = 20, wait_duration = 0.5)
+        goal = CountUntilGoal(max_numberf = 20, wait_duration = 0.5)            #z gory narzucamy jakie parametry zostana wyslane
         
         self._action_client.send_goal(goal, done_cb = self.done_callback, feedback_cb = self.feedback_callback)
         rospy.loginfo("Goal has been sent")
+        #dzieki funckja callback nie zawieszamy klienta w trakcie wykonywania operacji przez serwer, klient odwola sie do funkcji result i feedback jedynie jak serwer wysle wynik
         # self._action_client.wait_for_result()
         # rospy.loginfo(self._action_client.get_result())
     
