dcdc9fa0fe01801fe28b5d18629f58797b4601f2
GXR1LL4Z <crisza2001@gmail.com>
2022-10-30 18:09:30 +0100

    tb2

--- src/scripts/count_2_0/count_2_0_server.py
+++ src/scripts/count_2_0/count_2_0_server.py
@@ -12,11 +12,43 @@ class count_server:
         count_2_0Action,
         self.on_goal,
         auto_start=False)
+        #wystartowac serwe
         self.server.start()
-
-
+        rospy.loginfo("SERWER HAS BEEN STARTED")
+        
     def on_goal(self, goal):
-        pass
+        #wyswietlenie otzrymanego goala
+        rospy.loginfo("GOAL HAS BEEN RECEIVED: " + str(goal))
+        max_number = goal.max_number
+        wait_duration = goal.wait_duration
+        #czas spania
+        rate = rospy.Rate(1/wait_duration)
+        counter = 0
+        #flagi
+        succes = False
+        preemted = False
+        #feedback
+        feedback  = count_2_0Feedback()
+        while not rospy.is_shutdown:
+            counter += counter
+            if counter == max_number:
+                succes = True
+                break
+            if self.server.is_preempt_requested():
+                preemted = True
+                break
+            feedback = float(counter)/float(max_number)
+            self.server.publish_feedback(feedback)
+            rate.sleep()
+        result = count_2_0Result()
+        result = counter
+        if succes:
+            self.server.set_succeeded(result)
+        elif preemted:
+            self.server.set_preempted(result)
+        else:
+            self.server.set_aborted(result)
+
 if __name__ == '__main__':
     rospy.init_node('count_server',anonymous=True)
     server = count_server()
