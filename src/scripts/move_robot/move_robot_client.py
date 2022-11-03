838ebe68ef2824d284f759a479b18b7ede35166c
GXR1LL4Z <crisza2001@gmail.com>
2022-11-03 09:33:44 +0100

    tb2

---
+++ src/scripts/move_robot/move_robot_client.py
@@ -0,0 +1,8 @@
+import rospy
+
+class client:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    rospy.init_node('move_robot_client', anonymous = True)
---
+++ src/scripts/move_robot/move_robot_server.py
@@ -0,0 +1,9 @@
+import rospy
+
+class Server:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    rospy.init_node('move_robot_server', anonymous = True)
+    
