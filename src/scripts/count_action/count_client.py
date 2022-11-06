c1d28421161d9c21751ab8a64d0a4af0bec43f8b
GXR1LL4Z <crisza2001@gmail.com>
2022-11-06 20:07:59 +0100

    tb2

--- src/scripts/count_action/count_client.py
+++ src/scripts/count_action/count_client.py
@@ -1,4 +1,10 @@
 import rospy
+import actionlib
+
+from actions.msg import CountUntilAction
+from actions.msg import CountUntilFeedback
+from actions.msg import CountUntilGoal
+from actions.msg import CountUntilResult
 
 class server:
     def __init__(self):
--- src/scripts/count_action/count_server.py
+++ src/scripts/count_action/count_server.py
@@ -1,4 +1,10 @@
 import rospy
+import actionlib
+
+from actions.msg import CountUntilAction
+from actions.msg import CountUntilFeedback
+from actions.msg import CountUntilGoal
+from actions.msg import CountUntilResult
 
 class client:
     def __init__(self):
